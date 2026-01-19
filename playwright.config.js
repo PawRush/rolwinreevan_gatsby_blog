dbf0fa73248ffd5f78b569b5df34788e994b7b95
Sergey Kazantsev <sergeyka@amazon.com>
2026-01-19 13:36:47 +0100

    Support BASE_URL

--- playwright.config.js
+++ playwright.config.js
@@ -43,35 +43,11 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
-
-    /* Test against branded browsers */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
+    }
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  webServer: process.env.BASE_URL ? undefined : {
     command: 'npm run develop',
     port: 8001,
     timeout: 120 * 1000,
